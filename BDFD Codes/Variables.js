3af12b676561b37c712e11f418e005a266ca7eb2
Dark-Lynn <65414822+Dark-LYNN@users.noreply.github.com>
2021-07-30 23:00:49 +0200

    Create Variables.js

---
+++ BDFD Codes/Variables.js
@@ -0,0 +1,16 @@
+// Here you will see the variables i use for these open source codes
+
+// Note i use a javascript transformed file because it looks better on my github stats ;)
+// But its just
+// var {VarName) = "(Value)"; // Info about the variable
+
+import BDFD from "https://botdesignerdiscord.com/"
+
+// Main Variables
+var Prefix = "!"; // This is the most used prefix but change it to whatever prefix you use for your bot
+var Color = "ff7aed"; // This color is pink but you can change it to any color you want
+
+// Economy Variables
+var Money = 100 // This is the amount of money they start with in thier wallets.
+var Bank = 0 // This is the amount of money they start with in thier bank.
+var Currency = "<:Money:870771530391511130>" // This is the emote show when as curency you can change this to an emote , $ or any other curency  
