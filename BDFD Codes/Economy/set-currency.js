251c0bdac688055a6bcf5f2e69d688bd83e4a063
Dark-Lynn <65414822+Dark-LYNN@users.noreply.github.com>
2021-07-30 23:39:32 +0200

    Update and rename owo.md to set-currency.js

---
+++ BDFD Codes/Economy/set-currency.js
@@ -0,0 +1,40 @@
+// This is only needed if you want to be able change the curency.
+
+// Premium Version 
+
+trigger = $getServerVar[Prefix]set-currency
+
+code:`
+$ignoreTriggerCase
+$nomention
+
+$author[Currency Symbole changed]
+$description[**$username[$authorID]** has changed the Currency Symbole from $getServerVar[Currency] to $replaceText[$replaceText[$message;default;$getVar[Currency];1];default;;-1]]
+$thumbnail[$serverIcon[$guildID]]
+$setServerVar[Currency;$replaceText[$replaceText[$message;default;$getVar[Currency];1];default;;-1];$guildID]
+$color[$getVar[Color]]
+
+$onlyIf[$message[1]!=;:x: Wrong usage of $getServerVar[Prefix]setcurrency
+**Correct Usage:** $getServerVar[Prefix]setcurrency $getServerVar[Currency]
+**Note:** if you use $getServerVar[Prefix]setcurrency "default" it resets to the origional emote.]
+$onlyPerms[manageserver;:x: You need manageserver permissions]
+`
+
+// None Premium Version 
+
+trigger = !set-currency
+
+code:`
+$nomention
+
+$author[Currency Symbole changed]
+$description[**$username[$authorID]** has changed the Currency Symbole from $getServerVar[Currency] to $replaceText[$replaceText[$message;default;$getVar[Currency];1];default;;-1]]
+$thumbnail[$serverIcon[$guildID]]
+$setServerVar[Currency;$replaceText[$replaceText[$message;default;$getVar[Currency];1];default;;-1];$guildID]
+$color[$getVar[Color]]
+
+$onlyIf[$message[1]!=;:x: Wrong usage of $getServerVar[Prefix]setcurrency
+**Correct Usage:** $getServerVar[Prefix]setcurrency $getServerVar[Currency]
+**Note:** if you use $getServerVar[Prefix]setcurrency "default" it resets to the origional emote.]
+$onlyPerms[manageserver;:x: You need manageserver permissions]
+`
